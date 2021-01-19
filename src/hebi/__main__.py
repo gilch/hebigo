db53554a0cfea57c6470b1f492fc394b326d22f9
gilch <gilch@users.noreply.github.com>
2021-01-18 21:25:41 -0700

    Start REPL without using pid

--- src/hebi/__main__.py
+++ src/hebi/__main__.py
@@ -3,20 +3,22 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import subprocess
+import sys
 import time
 
 from jupyter_console import app
 
 
 def main():
-    kernel = subprocess.Popen(["python", "-m", "hebi.kernel"])
-
-    time.sleep(1)  # TODO: this seems brittle.
-    app.launch_new_instance(
-        argv=["jupyter", "console", "--existing", f"kernel-{kernel.pid}.json"]
-    )
-    print("killing kernel")
+    print("Attempting to start Hebigo kernel without installing kernelspec.")
+    kernel = subprocess.Popen([sys.executable, "-m", "hebi.kernel"])
+    print("Waiting for kernel to start.")
+    time.sleep(3)  # TODO: this seems brittle.
+    print("Starting Jupyter Console with the most recent active kernel...")
+    app.launch_new_instance(argv=["jupyter", "console", "--existing"])
+    print("Console exit. Terminating Hebigo kernel.")
     kernel.kill()
 
 
-main()
+if __name__ == "__main__":
+    main()
